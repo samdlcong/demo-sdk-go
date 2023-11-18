220b127bda847f2c26ac12d622c161f83070b81f
samdlcong <samnew@126.com>
2023-11-18 10:54:19 +0800

    add response.go

--- go.mod
+++ go.mod
@@ -2,6 +2,14 @@ module github.com/samdlcong/demo-sdk-go
 
 go 1.21.3
 
-require github.com/sirupsen/logrus v1.9.3
+require (
+	github.com/marmotedu/component-base v1.6.2
+	github.com/sirupsen/logrus v1.9.3
+)
 
-require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+require (
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- sdk/response/response.go
+++ sdk/response/response.go
@@ -1,1 +1,51 @@
 package response
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/samdlcong/demo-sdk-go/sdk/errors"
+	"io"
+	"net/http"
+)
+
+type Response interface {
+	ParseErrorFromHTTPResponse(body []byte) error
+}
+
+type ErrorResponse struct {
+	Code      int    `json:"code"`
+	Message   string `json:"message,omitempty"`
+	RequestID string `json:"requestID,omitempty"`
+}
+
+type BaseResponse struct {
+	ErrorResponse
+}
+
+func (r *BaseResponse) ParseErrorFromHTTPResponse(body []byte) error {
+	if err := json.Unmarshal(body, r); err != nil {
+		return err
+	}
+	if r.Code > 0 {
+		return errors.NewDEMOSDKError(r.Code, r.Message, r.RequestID)
+	}
+
+	return nil
+}
+
+func ParseFromHttpResponse(rawResponse *http.Response, response Response) error {
+	defer rawResponse.Body.Close()
+	body, err := io.ReadAll(rawResponse.Body)
+	if err != nil {
+		return err
+	}
+	if rawResponse.StatusCode != 200 {
+		return fmt.Errorf("request fail with status: %s, with body: %s", rawResponse.Status, body)
+	}
+
+	if err := response.ParseErrorFromHTTPResponse(body); err != nil {
+		return err
+	}
+
+	return json.Unmarshal(body, &response)
+}
