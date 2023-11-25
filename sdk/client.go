0e8352eecffdd8529efa4f86ba511970d5409abb
samdlcong <samnew@126.com>
2023-11-25 10:26:14 +0800

    add client

--- sdk/client.go
+++ sdk/client.go
@@ -1,8 +1,11 @@
 package sdk
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/samdlcong/demo-sdk-go/sdk/log"
+	"github.com/samdlcong/demo-sdk-go/sdk/request"
+	"github.com/samdlcong/demo-sdk-go/sdk/response"
 	"net/http"
 	"runtime"
 	"strings"
@@ -50,3 +53,85 @@ func (c *Client) WithConfig(config *Config) *Client {
 	c.Logger.SetLevel(config.LogLevel)
 	return c
 }
+
+func (c *Client) Send(req request.Request, resp response.Response) error {
+	method := req.GetMethod()
+	builder := GetParameterBuilder(method, c.Logger)
+	jsonReq, _ := json.Marshal(req)
+	encodedUrl, err := builder.BuildURL(req.GetURL(), jsonReq)
+	if err != nil {
+		return err
+	}
+
+	endPoint := c.Config.Endpoint
+	if endPoint == "" {
+		endPoint = fmt.Sprintf("%s/%s", defaultEndpoint, c.ServiceName)
+	}
+	reqUrl := fmt.Sprintf("%s://%s/%s%s", c.Config.Scheme, endPoint, req.GetVersion(), encodedUrl)
+
+	body, err := builder.BuildBody(jsonReq)
+	if err != nil {
+		return err
+	}
+
+	sign := func(r *http.Request) error {
+		signer := NewSigner(c.signMethod, c.Credential, c.Logger)
+		_ = signer.Sign(c.ServiceName, r, strings.NewReader(body))
+		return err
+	}
+
+	rawResponse, err := c.doSend(method, reqUrl, body, req.GetHeaders(), sign)
+	if err != nil {
+		return err
+	}
+
+	return response.ParseFromHttpResponse(rawResponse, resp)
+}
+
+func (c *Client) doSend(method, url, data string, header map[string]string, sign SignFunc) (*http.Response, error) {
+	client := &http.Client{Timeout: c.Config.Timeout}
+
+	req, err := http.NewRequest(method, url, strings.NewReader(data))
+	if err != nil {
+		c.Logger.Errorf("%s", err.Error())
+		return nil, err
+	}
+
+	c.setHeader(req, header)
+
+	err = sign(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return client.Do(req)
+}
+
+func (c *Client) setHeader(req *http.Request, header map[string]string) {
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("User-Agent", defaultUserAgent)
+
+	for k, v := range header {
+		req.Header.Set(k, v)
+	}
+
+	for k, v := range req.Header {
+		c.Logger.Infof("header key: %s, header value: %s", k, v)
+	}
+}
+
+// Bool is a helper routine that allocates a new bool value
+// to store v and returns a pointer to it.
+func Bool(v bool) *bool { return &v }
+
+// Int is a helper routine that allocates a new int value
+// to store v and returns a pointer to it.
+func Int(v int) *int { return &v }
+
+// Int64 is a helper routine that allocates a new int64 value
+// to store v and returns a pointer to it.
+func Int64(v int64) *int64 { return &v }
+
+// String is a helper routine that allocates a new string value
+// to store v and returns a pointer to it.
+func String(v string) *string { return &v }
