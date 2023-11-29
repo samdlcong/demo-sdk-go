43ec37c86fd26c398cafa810e8b2398e78a1705f
samdlcong <samnew@126.com>
2023-11-29 16:08:27 +0800

    add types.go

--- services/common/meta/v1/types.go
+++ services/common/meta/v1/types.go
@@ -18,3 +18,52 @@ type ObjectMeta struct {
 	CreatedAt time.Time `json:"createdAt,omitempty" gorm:"column:createdAt"`
 	UpdatedAt time.Time `json:"updatedAt,omitempty" gomrm:"column:updatedAt"`
 }
+
+type ListOptions struct {
+	TypeMeta       `json:",inline"`
+	LabelSelector  string `json:"labelSelector,omitempty" from:"labelSelector"`
+	FieldSelector  string `json:"fieldSelector,omitempty" from:"fieldSelector"`
+	TimeoutSeconds *int64 `json:"timeoutSeconds,omitempty"`
+	Offset         *int64 `json:"offset,omitempty" from:"offset"`
+	Limit          *int64 `json:"limit,omitempty" from:"limit"`
+}
+
+type ExportOptions struct {
+	TypeMeta `json:",inline"`
+	Export   bool `json:"export"`
+	Exact    bool `json:"exact"`
+}
+
+type GetOptions struct {
+	TypeMeta `json:",inline"`
+}
+
+type DeleteOptions struct {
+	TypeMeta `json:",inline"`
+	Unscoped bool `json:"unscoped"`
+}
+
+type CreateOptions struct {
+	TypeMeta `json:",inline"`
+	DryRun   []string `json:"dryRun,omitempty"`
+}
+
+type PatchOptions struct {
+	TypeMeta `json:",inline"`
+	DryRun   []string `json:"dryRun,omitempty"`
+	Force    bool     `json:"force,omitempty"`
+}
+
+type UpdateOptions struct {
+	TypeMeta `json:",inline"`
+	DryRun   []string `json:"dryRun,omitempty"`
+}
+
+type AuthorizeOptions struct {
+	TypeMeta `json:",inline"`
+}
+
+type TableOptions struct {
+	TypeMeta  `json:",inline"`
+	NoHeaders bool `json:"-"`
+}
